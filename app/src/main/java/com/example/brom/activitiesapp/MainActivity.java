d4de7d725a566f0cbe491d083507c66df2178f74
a18oscte <a18oscte@student.his.se>
2019-04-16 14:28:03 +0200

    lagt in min kod från uppgift 2 och skapat en ny activity.

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.brom.activitiesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
@@ -14,11 +19,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        List<String> listData = new ArrayList<>();
+
+        final ArrayList<Mountain> mountainArr = new ArrayList();
+
+        Mountain m = new Mountain("Kebnekaise", "Skanderna", 2106);
+
+        mountainArr.add(m);
+
+        for(int i = 0; i < mountainNames.length; i++) {
+            mountainArr.add(new Mountain(mountainNames[i], mountainLocations[i], mountainHeights[i]));
+        }
+
+        for(int i = 0; i < mountainArr.size(); i++) {
+
+            listData.add(mountainArr.get(i).namn());
+        }
+
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,R.id.list_item_textview,listData);
+
+
+
+
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+
         // 1. Create a ListView as in previous assignment
         // 2. Create a new activity named "MountainDetailsActivity
         // 3. Create a new Layout file for the MountainDetailsActivity called
         //    "activity_mountaindetails". MountainDetailsActivity must have MainActivity as its
-        //    ´parent activity.
+        //        //    ´parent activity.
         // 4. The layout file created in step 3 must have a LinearLayout
         // 5. The layout file created in step 3 must be able to display
         //    * Mountain Name
